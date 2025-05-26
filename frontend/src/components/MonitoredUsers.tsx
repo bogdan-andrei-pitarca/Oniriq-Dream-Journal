305fd6e9c0f9eede5751eaadedbab960d0cff1bb
bogdan-andrei-pitarca <bogdanandreipitarca@outlook.com>
2025-05-26 19:01:39 +0300

    fixed import

--- frontend/src/components/MonitoredUsers.tsx
+++ frontend/src/components/MonitoredUsers.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useAuth } from '../hooks/useAuth';
+import { useAuth } from '../app/context/AuthContext';
 
 interface User {
   id: number;

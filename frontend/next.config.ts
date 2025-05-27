7ea7124521c0b73e485bc6cc463fad71eb1be064
bogdan-andrei-pitarca <bogdanandreipitarca@outlook.com>
2025-05-27 13:49:22 +0300

    fixed frontend dockerfile conflict

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,27 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `https://${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
-      },
-    ];
-  },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
-        ],
-      },
-    ];
-  },
-};
+/** @type {import('next').NextConfig} */
+    const nextConfig = {
+      output: 'standalone', // if you want standalone output
+      env: {
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
+      }
+    }
 
 export default nextConfig;

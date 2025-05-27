d8e1fe6aa21de7cc963297fdba0dbcd37b9ed667
bogdan-andrei-pitarca <bogdanandreipitarca@outlook.com>
2025-05-27 20:25:07 +0300

    changed next config again

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
     const nextConfig = {
-      output: 'standalone', // if you want standalone output
       env: {
-        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+        NEXT_PUBLIC_WEBSOCKET_URL: process.env.NEXT_PUBLIC_WEBSOCKET_URL
       }
     }
 
